e75a3f29226261def088e32fa775cc0819053294
eerickson07 <91229396+eerickson07@users.noreply.github.com>
2022-07-22 14:14:32 -0500

    Final WhatABook Code

--- module_12/what_a_book.py
+++ module_12/what_a_book.py
@@ -3,7 +3,7 @@ import sys
 import mysql.connector
 from mysql.connector import errorcode
 
-
+#After not being able to get my original code operating I used some of the formatting and code in the csd-310 GitHub to complete the program.
 config = {
     "user": "whatabook_user",
     "password": "MySQL8IsGreat!",
@@ -18,23 +18,25 @@ def show_menu():
     print("   1. View Books\n   2. View Store Locations\n   3. My Account\n   4 Exit Program")
 
     try:
-        choice = int(input('  '))
+        choice = int(input(' Please select a choice from the menu '))
 
         return choice
     except ValueError:
         print(" Invalid Choice, please try again")
+
+        sys.exit(0)
 
 #creating the book menu
 def show_books(_cursor):
     #Query for all the book data
     _cursor.execute("SELECT book_id, book_name, author, details from book")
 
-    books = _cursor.fetchall
+    books = _cursor.fetchall()
 
     print("\n  BOOK LIST")
 
     for book in books:
-        print("Book Name: {} \n Author: {} \n Details: {}\n".format(books[0], books[1], books[2]))
+        print(" Book Name: {}\n Author: {}\n Details: {}\n".format(book[0], book[1], book[2]))
 
 #creating the location menu
 
@@ -56,18 +58,19 @@ def confirm_user ():
         if user_id < 0 or user_id > 3:
             print("Incorrect customer ID number, please try again")
             sys.exit(0)
-
+        else:
             return user_id
     except ValueError:
             print("\n Invalid customer ID, please try again")
 
             sys.exit(0)
 
+#Creating a function to show the Menu
 def show_account_menu():
     try:
-        print("Whatabook Menu")
+        print("WhatABook Menu")
         print(" 1. Wishlist\n   2. Add Book\n    3. Main Menu")
-        account_option = int(input(" Please select option: "))
+        account_option = int(input(" Please select option from our menu: "))
 
         return account_option
 
@@ -78,17 +81,19 @@ def show_account_menu():
         sys.exit(0)
 
 #creating a query for the database
-def show_wishlist(_cursor, _user_id):
-    _cursor.execute("SELECT user.user_id, user.first_name, user.last_name, book.book_id, book.book_name, book.author" +
-    "FROM wishlist" + "INNER JOIN book ON wishlist.book_id = book.book_id" + "INNER JOIN user ON wishlist.user_id + user.user_id"+
-    "WHERE user.user_id = {}".format(_user_id))
+def show_wishlist(_cursor, user_id):
+    _cursor.execute("SELECT user.user_id, user.first_name, user.last_name, book.book_id, book.book_name, book.author " + 
+                    "FROM wishlist " + 
+                    "INNER JOIN user ON wishlist.user_id = user.user_id " + 
+                    "INNER JOIN book ON wishlist.book_id = book.book_id " + 
+                    "WHERE user.user_id = {}".format(user_id))
 
     wishlist = _cursor.fetchall()
 
     print ("\n WISHLIST ITEMS")
 
     for book in wishlist:
-        print("Book Name: {}\n   Author:{}\n".format(book[4], book[5]))
+        print("Book Name: {}\n   Author:{}\n".format(book[3], book[4], book[5]))
 
 #creating a query for list of books not on the wishlist
 
@@ -123,6 +128,8 @@ try:
     print("Welcome to WhatABook App!")
 
     user_option = show_menu()  #calling the function to show user the menu
+
+    account = show_account_menu
 
     while user_option !=4:
         if user_option == 1:   #calling the show_books function if user selects it
